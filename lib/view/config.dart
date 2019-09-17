25d8322da54602331f1089e0b4763c5aa7eade64
MaelSantos <maelsantos777@gmail.com>
2019-09-17 14:26:20 -0300

    TELA DE CONFIGURAÇÃO

--- lib/view/config.dart
+++ lib/view/config.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:log_man/util/tela.dart';
 import 'package:log_man/view/menu.dart';
 
-class Config extends StatefulWidget{
-
+class Config extends StatefulWidget {
   MenuState menu;
   Config(this.menu);
 
@@ -11,18 +10,43 @@ class Config extends StatefulWidget{
   State<StatefulWidget> createState() {
     return ConfigState(menu);
   }
-
 }
 
 class ConfigState extends State<Config> {
   MenuState menu;
-  double values = 0.0;
+  Image musica, audio, veloci;
+  double velocidade;
+  double volume;
+  bool inVolume;
 
   ConfigState(this.menu);
 
   @override
-  Widget build(BuildContext context) {
+  void initState() {
+    super.initState();
 
+    velocidade = 50.0;
+    volume = 50.0;
+    inVolume = true;
+
+    if (inVolume)
+      musica = Image.asset("assets/icons/musicaOn.png");
+    else
+      musica = Image.asset("assets/icons/musicaOff.png");
+
+    if (velocidade > 50.0)
+      veloci = Image.asset("assets/icons/velocidade.png");
+    else
+      veloci = Image.asset("assets/icons/iniciar.png");
+
+    if (volume != 0)
+      audio = Image.asset("assets/icons/audioOn.png");
+    else
+      audio = Image.asset("assets/icons/audioOff.png");
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(10.0),
       constraints: BoxConstraints.expand(),
@@ -35,41 +59,69 @@ class ConfigState extends State<Config> {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Slider(
-            value: values,
-            min: 0,
-            max: 10,
-            onChanged: (va){
-              setState(() {
-                values = va;
-              });
-            },
+          Row(
+            children: [
+              audio,
+              Slider(
+                activeColor: Colors.indigoAccent,
+                label: volume.toInt().toString() + "%",
+                value: volume,
+                min: 0,
+                max: 100,
+                divisions: 10,
+                onChanged: (va) {
+                  setState(() {
+                    volume = va;
+                    if (va != 0)
+                      audio = Image.asset("assets/icons/audioOn.png");
+                    else
+                      audio = Image.asset("assets/icons/audioOff.png");
+                  });
+                },
+              ),
+            ],
           ),
-          Slider(
-            value: values,
-            min: 0,
-            max: 10,
-            onChanged: (va){
-              setState(() {
-                values = va;
-              });
+          Row(
+            children: <Widget>[
+              veloci,
+              Slider(
+                activeColor: Colors.indigoAccent,
+                label: velocidade.toInt().toString() + "%",
+                value: velocidade,
+                min: 0,
+                max: 100,
+                divisions: 10,
+                onChanged: (va) {
+                  setState(() {
+                    velocidade = va;
+                    if (va > 50.0)
+                      veloci = Image.asset("assets/icons/velocidade.png");
+                    else
+                      veloci = Image.asset("assets/icons/iniciar.png");
+                  });
+                },
+              ),
+            ],
+          ),
+          FlatButton(
+            onPressed: () {
+              setState(() {});
+
+              if (inVolume) {
+                musica = Image.asset("assets/icons/musicaOn.png");
+                inVolume = false;
+              } else {
+                musica = Image.asset("assets/icons/musicaOff.png");
+                inVolume = true;
+              }
             },
+            child: musica,
           ),
           FlatButton(
               onPressed: () {
                 menu.transicao(Tela.principal);
               },
               child: Image.asset("assets/icons/home.png")),
-          FlatButton(
-              onPressed: () {
-                menu.transicao(Tela.principal);
-              },
-              child: Image.asset("assets/icons/musicaOn.png")),
-          FlatButton(
-              onPressed: () {
-                menu.transicao(Tela.principal);
-              },
-              child: Image.asset("assets/icons/salvar.png")),
         ],
       )),
     );
